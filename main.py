c48ed04fe5ef0159e22663310c207a721f20f481
gwb <gw.basse@gmail.com>
2025-07-27 17:00:44 +0100

    sketch out terminfo parser.

---
+++ main.py
@@ -0,0 +1,253 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+# See: https://github.com/mirror/ncurses/blob/87c2c84cbd2332d6d94b12a1dcaf12ad1a51a938/include/Caps
+# for order of boolean capabilities
+
+BOOL_CAPS = [
+    'bw',
+    'am',
+    'xsb',
+    'xhp',
+    'xenl',
+    'eo',
+    'gn',
+    'hc',
+    'km',
+    'hs',
+    'in',
+    'da',
+    'db',
+    'mir',
+    'msgr',
+    'os',
+    'eslok',
+    'xt',
+    'hz',
+    'ul',
+    'xon',
+    'nxon',
+    'mc5i',
+    'chts',
+    'nrrmc',
+    'npc',
+    'ndscr',
+    'ccc',
+    'bce',
+    'hls',
+    'xhpa',
+    'crxm',
+    'daisy',
+    'xvpa',
+    'sam',
+    'cpix',
+    'lpix',
+]
+
+NUM_CAPS = [
+    'cols',
+    'it',
+    'lines',
+    'lm',
+    'xmc',
+    'pb',
+    'vt',
+    'wsl',
+    'nlab',
+    'lh',
+    'lw',
+    'ma',
+    'wnum',
+    'colors',
+    'pairs',
+    'ncv',
+    'bufsz',
+    'spinv',
+    'spinh',
+    'maddr',
+    'mjump',
+    'mcs',
+    'mls',
+    'npins',
+    'orc',
+    'orl',
+    'orhi',
+    'orvi',
+    'cps',
+    'widcs',
+    'btns',
+    'bitwin',
+    'bitype'
+]
+
+
+@dataclass
+class Header:
+    magic_number: str
+    names_nb: int
+    boolean_nb: int
+    numbers_nsi: int
+    strings_nsi: int
+    string_table_nb: int
+
+def parse_caps_file():
+    with open('caps', 'r') as f: content = f.readlines()
+
+    keep = []
+    for line in content:
+        if "%%-STOP-HERE-%%" in line:
+            break
+        if not line.startswith('#'):
+            keep.append([o for o in line.split('\t') if o != ''])
+    
+    keep_bool = [
+        elt[1] for elt in keep if any(o == 'bool' for o in elt)
+    ]
+
+    keep_num = [
+        elt[1] for elt in keep if any(o == 'num' for o in elt)
+    ]
+
+
+    keep_string = [
+        elt[1] for elt in keep if any(o == 'str' for o in elt)
+    ]
+    return {'bool': keep_bool, 'num': keep_num, 'string': keep_string}
+
+
+def load_terminfo_raw()->bytes:
+    path = Path("/Applications/Ghostty.app/Contents/Resources/terminfo/78/xterm-ghostty")
+    with open(path, 'rb') as f:
+        tinf = f.read()
+    return tinf
+
+def get_header(tinf: bytes)->Header:
+    header_vals = []
+    for i in range(6):
+        if i == 0:
+            header_vals.append(oct(tinf[2*i] + tinf[2*i+1] * 256))
+        else:
+            header_vals.append(tinf[2*i] + tinf[2*i+1] * 256)
+    return Header(*header_vals)
+
+def section_names(tinf: bytes, header: Header)->list[str]:
+    # the -1 is used to remove the ASCII NUL (\x00) that terminates
+    # the section.
+    names = tinf[12:12+header.names_nb-1].decode('ascii')
+    return names.split('|')
+
+def section_bool(tinf: bytes, header: Header)->list[str]:
+    start = 12+header.names_nb
+    return sorted(
+        [
+            cap for flag, cap in zip(tinf[start:start+header.boolean_nb+1], BOOL_CAPS)
+            if flag == 1
+        ]
+    )
+
+def section_numbers(tinf: bytes, header: Header):
+    # possibly skips a byte so that number section starts on even byte
+    start = 12 + header.names_nb + header.boolean_nb
+    if start % 2 == 1:
+        assert tinf[start] == 0, "Expected null byte as offset"
+        start += 1
+
+    nums = []
+    for i in range(header.numbers_nsi):
+        blo, bhi = tinf[start+2*i], tinf[start+2*i+1]
+        if oct(blo) == '0o377' and oct(bhi) == '0o377':
+            # capabilities is missing
+            continue
+        nums.append((NUM_CAPS[i], blo + 256*bhi))
+
+    return sorted(nums, key=lambda x: x[0])
+    
+def section_strings(tinf: bytes, header: Header, strings_cap: list[str]):
+    start = (
+        12 +
+        header.names_nb +
+        header.boolean_nb +
+        2 * header.numbers_nsi
+    )
+
+    if start % 2 == 1:
+        start += 1
+
+    # first, load the strings table
+    start_alt = start + 2 * header.strings_nsi
+    strings_table = tinf[start_alt:]
+    #breakpoint()
+
+    # index into the string table
+    res = []
+    for i in range(header.strings_nsi):
+        print(i)
+        blo, bhi = tinf[start+2*i], tinf[start+2*i+1]
+        if oct(blo) == '0o377' and oct(bhi) == '0o377':
+            # capabilities is missing
+            continue
+        offset = blo + 256*bhi
+        print(f'offset = {offset}')
+        ii = 0
+        try:
+            while strings_table[offset+ii] != 0:
+                print(ii)
+                ii += 1
+        except:
+            breakpoint()
+        
+        val = strings_table[offset:offset+ii]
+        res.append((strings_cap[i], val))
+    return res
+        # nums.append((NUM_CAPS[i], ))
+    
+    
+
+
+if __name__ == '__main__':
+
+    tinf = load_terminfo_raw()
+
+    header = get_header(tinf) # reads first 12 bytes (0-11)
+    names = section_names(tinf, header)
+    
+    bools = section_bool(tinf, header)
+
+
+    start_alt = (
+        12 +
+        header.names_nb +
+        header.boolean_nb +
+        2 * header.numbers_nsi +
+        2 * header.strings_nsi
+    )
+    strtbl = tinf[start_alt:start_alt+20]
+    caps = parse_caps_file()    
+    a = section_strings(tinf, header, caps['string'])
+
+    start_alt = (
+        12 +
+        header.names_nb +
+        header.boolean_nb +
+        2 * header.numbers_nsi +
+        1 +
+        2 * header.strings_nsi
+    )
+
+    strings_table = tinf[start_alt:]
+
+    # index into the string table
+    start = 12 + header.names_nb + header.boolean_nb + 2 * header.numbers_nsi + 1
+
+    lo, hi = tinf[start], tinf[start+1]
+
+    b = []
+    for (cap, cp) in a:
+        if len(cp) == 1 and ord(cp) <=31:
+            b.append((cap, '^{cp}'.format(cp=chr(ord(cp)+0x40))))
+        else:
+            # if any element of the list of codepoints is '\x1b',
+            # convert to the \E representation
+            cpstr = ''.join(['\E' if cp_i == 27 else chr(cp_i) for cp_i in cp])
+            b.append((cap, cpstr))
+
